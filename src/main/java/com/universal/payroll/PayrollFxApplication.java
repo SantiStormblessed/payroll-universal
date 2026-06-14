3eb859fa01465eb83ab81fb90b31fb55a9ee4dd1
SantiStormblessed <santi.ssf@gmail.com>
2026-06-14 12:58:48 -0500

    remove unused imports

--- src/main/java/com/universal/payroll/PayrollFxApplication.java
+++ src/main/java/com/universal/payroll/PayrollFxApplication.java
@@ -3,9 +3,7 @@ package com.universal.payroll;
 import com.universal.payroll.config.FxmlView;
 import com.universal.payroll.config.StageManager;
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
